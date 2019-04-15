75efdb756cc1ce9fe68c4692a7df806c90b1ce10
German Guzelj <german.guzelj@gmail.com>
2019-04-15 21:01:23 +0100

    Fixing bug on ElementFetcher (returning multiple times the same element)

--- src/main/java/com/agileengine/ElementFetcher.java
+++ src/main/java/com/agileengine/ElementFetcher.java
@@ -3,19 +3,20 @@ package com.agileengine;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Attribute;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.nodes.Node;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.toSet;
 
 public class ElementFetcher {
 
@@ -38,24 +39,24 @@ public class ElementFetcher {
                 CHARSET_NAME,
                 htmlFile.getAbsolutePath());
 
-            return attributes.keySet().stream()
-                .map(key -> findElementsByAttribute(doc, key, attributes.get(key)))
-                .flatMap(Collection::stream)
-                .collect(toList());
+            HashSet<Element> fetchedElements = new HashSet<>();
 
+            attributes.forEach((key, value) -> {
+                fetchedElements.addAll(findElementsByAttribute2(doc, key, attributes.get(key)));
+            });
+
+            return fetchedElements.stream()
+                .map(Node::attributes)
+                .map(elementAttribute -> elementAttribute.asList().stream().collect(toMap(Attribute::getKey, Attribute::getValue)))
+                .map(ElementDescriptor::new)
+                .collect(toList());
         } catch (IOException e) {
             LOGGER.error("Error reading [{}] file", htmlFile.getAbsolutePath(), e);
             return new ArrayList<>();
         }
     }
 
-    private static List<ElementDescriptor> findElementsByAttribute(Document doc, String key, String value) {
-        return doc.getElementsByAttributeValue(key, value).stream()
-            .map(Node::attributes)
-            .map(elementAttribute -> elementAttribute.asList().stream().collect(toMap(Attribute::getKey, Attribute::getValue)))
-            .map(ElementDescriptor::new)
-            .collect(toList());
+    private static Set<Element> findElementsByAttribute2(Document doc, String key, String value) {
+        return new HashSet<>(doc.getElementsByAttributeValue(key, value));
     }
-
-
 }
